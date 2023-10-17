e46ae8137bb5048e33616fb95070e584baf1e64d
mohadebDev <maxff00789@gmail.com>
2023-10-18 04:25:26 +0600

    added all brand collection with post

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+require('dotenv').config()
 
 
 // use middleWare 
 
 app.use(cors());
 app.use(express.json())
+
 
 
 // mongoDB connection 
@@ -26,6 +28,52 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+    const brandsDB = client.db('brandsDB');
+    const appleCollection = brandsDB.collection('appleCollection');
+    const xiaomiCollection = brandsDB.collection('xiaomiCollection');
+    const samsungCollection = brandsDB.collection('samsungCollection');
+    const sonyCollection = brandsDB.collection('sonyCollection');
+    const googleCollection = brandsDB.collection('googleCollection');
+    const asusCollection = brandsDB.collection('asusCollection');
+
+
+    app.post('/brand/apple', async (req, res) => {
+      const newProduct = req.body;
+      const result = await appleCollection.insertOne(newProduct);
+      res.send(result);
+    })
+
+    app.post('/brand/xiaomi', async (req, res) => {
+      const newProduct = req.body;
+      const result = await xiaomiCollection.insertOne(newProduct);
+      res.send(result);
+    })
+
+    app.post('/brand/samsung', async (req, res) => {
+      const newProduct = req.body;
+      const result = await samsungCollection.insertOne(newProduct);
+      res.send(result);
+    })
+
+    app.post('/brand/sony', async (req, res) => {
+      const newProduct = req.body;
+      const result = await sonyCollection.insertOne(newProduct);
+      res.send(result);
+    })
+
+    app.post('/brand/google', async (req, res) => {
+      const newProduct = req.body;
+      const result = await googleCollection.insertOne(newProduct);
+      res.send(result);
+    })
+
+    app.post('/brand/asus', async (req, res) => {
+      const newProduct = req.body;
+      const result = await asusCollection.insertOne(newProduct);
+      res.send(result);
+    })
+
+
     
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
